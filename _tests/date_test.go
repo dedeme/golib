a45e1d0976b169d51f0cb56376114abbfa195e01
Deme <deme@debian.es>
2020-04-06 12:33:54 +0200

    Added date

---
+++ _tests/date_test.go
@@ -0,0 +1,144 @@
+// Copyright 07-Apr-2020 ºDeme
+// GNU General Public License - V3 <http://www.gnu.org/licenses/>
+
+package _tests
+
+import (
+	"github.com/dedeme/golib/date"
+	"sort"
+	"testing"
+)
+
+func TestToFrom(t *testing.T) {
+	d := date.Now()
+	if r := eqi(len(d.String()), 8); r != "" {
+		t.Fatal(r)
+	}
+
+	d = date.New(2, 4, 2010)
+	if r := eq(d.String(), "20100402"); r != "" {
+		t.Fatal(r)
+	}
+	if r := eq(d.ToIso(), "02/04/2010"); r != "" {
+		t.Fatal(r)
+	}
+	if r := eq(d.ToEn(), "04/02/2010"); r != "" {
+		t.Fatal(r)
+	}
+
+	d1 := date.FromEn("12/31/1988", "/")
+	d2 := date.FromIso("31/12/1988", "/")
+	if !d1.Eq(d2) {
+		t.Fatal(fail)
+	}
+	if r := eq(d1.String(), "19881231"); r != "" {
+		t.Fatal(r)
+	}
+	if r := eq(d1.ToIso(), "31/12/1988"); r != "" {
+		t.Fatal(r)
+	}
+	if r := eq(d2.ToEn(), "12/31/1988"); r != "" {
+		t.Fatal(r)
+	}
+
+	d1 = date.NewTime(2, 4, 2010, 12, 40, 15)
+	if r := eq(d.String(), "20100402"); r != "" {
+		t.Fatal(r)
+	}
+	if r := eq(d1.Format("%t"), "12:40:15"); r != "" {
+		t.Fatal(r)
+	}
+	if r := eq(d1.Format("%T in %%"), "12:40:15.000 in %"); r != "" {
+		t.Fatal(r)
+	}
+}
+
+func TestOperations(t *testing.T) {
+	d1 := date.New(29, 2, 2013)
+	d2 := date.New(6, 3, 2013)
+	d3 := date.New(30, 4, 2013)
+
+	if r := eqi(d1.Day(), 1); r != "" {
+		t.Fatal(r)
+	}
+	if r := eqi(d1.Month(), 3); r != "" {
+		t.Fatal(r)
+	}
+	if r := eqi(d1.Year(), 2013); r != "" {
+		t.Fatal(r)
+	}
+	if r := eqi(d1.Hour(), 12); r != "" {
+		t.Fatal(r)
+	}
+	if r := eqi(d1.Minute(), 0); r != "" {
+		t.Fatal(r)
+	}
+	if r := eqi(d1.Second(), 0); r != "" {
+		t.Fatal(r)
+	}
+
+	if d1.Eq(d2) {
+		t.Fatal(fail)
+	}
+	if d1.Compare(d2) >= 0 {
+		t.Fatal(fail)
+	}
+	if r := eqi(d1.Df(d2), -5); r != "" {
+		t.Fatal(r)
+	}
+
+	if d3.Eq(d2) {
+		t.Fatal(fail)
+	}
+	if d3.Compare(d2) <= 0 {
+		t.Fatal(fail)
+	}
+	if r := eqi(d3.Df(d2), 55); r != "" {
+		t.Fatal(r)
+	}
+
+	if r := eqi(d1.Add(25).Add(-25).Df(d1), 0); r != "" {
+		t.Fatal(r)
+	}
+	if r := eqi(d1.Add(25).Add(-30).Df(d1), -5); r != "" {
+		t.Fatal(r)
+	}
+	if r := eqi(d1.Add(25).Add(-20).Df(d1), 5); r != "" {
+		t.Fatal(r)
+	}
+
+	if r := eqi(d1.AddSeconds(25).AddSeconds(-25).DfTime(d1), 0); r != "" {
+		t.Fatal(r)
+	}
+	if r := eqi(d1.AddSeconds(25).AddSeconds(-30).DfTime(d1), -5000); r != "" {
+		t.Fatal(r)
+	}
+	if r := eqi(d1.AddSeconds(25).AddSeconds(-20).DfTime(d1), 5000); r != "" {
+		t.Fatal(r)
+	}
+
+	ds := []date.T{d3, d1, d2}
+	sort.Sort(date.Sorter(ds))
+	if !ds[0].Eq(d1) {
+		t.Fatal(fail)
+	}
+	if !ds[1].Eq(d2) {
+		t.Fatal(fail)
+	}
+	if !ds[2].Eq(d3) {
+		t.Fatal(fail)
+	}
+
+	ds = []date.T{d3, d1, d2}
+	sort.Sort(date.SorterTime(ds))
+	if !ds[0].Eq(d1) {
+		t.Fatal(fail)
+	}
+	if !ds[1].Eq(d2) {
+		t.Fatal(fail)
+	}
+	if !ds[2].Eq(d3) {
+		t.Fatal(fail)
+	}
+
+}
---
+++ date/date.go
@@ -0,0 +1,348 @@
+// Copyright 07-Apr-2020 ºDeme
+// GNU General Public License - V3 <http://www.gnu.org/licenses/>
+
+// Date - time management
+package date
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func toMonth(m int) time.Month {
+	switch m {
+	case 1:
+		return time.January
+	case 2:
+		return time.February
+	case 3:
+		return time.March
+	case 4:
+		return time.April
+	case 5:
+		return time.May
+	case 6:
+		return time.June
+	case 7:
+		return time.July
+	case 8:
+		return time.August
+	case 9:
+		return time.September
+	case 10:
+		return time.October
+	case 11:
+		return time.November
+	case 12:
+		return time.December
+	default:
+		panic(fmt.Sprintf("%v is not a valid month", m))
+	}
+}
+
+func fromMonth(m time.Month) int {
+	switch m {
+	case time.January:
+		return 1
+	case time.February:
+		return 2
+	case time.March:
+		return 3
+	case time.April:
+		return 4
+	case time.May:
+		return 5
+	case time.June:
+		return 6
+	case time.July:
+		return 7
+	case time.August:
+		return 8
+	case time.September:
+		return 9
+	case time.October:
+		return 10
+	case time.November:
+		return 11
+	default:
+		return 12
+	}
+}
+
+type T time.Time
+
+func Now() T {
+	return T(time.Now())
+}
+
+// 'day' is between 1 and 12 both inclusive. It it is out of range, date rolls
+// to the corresponding valid date.
+// 'month' is between 1 and 12 both inclusive. If month is out of range, a
+// 'panic' is raised.
+func New(day, month, year int) T {
+	return NewTime(day, month, year, 12, 0, 0)
+}
+
+// 'day' is between 1 and 12 both inclusive. It it is out of range, date rolls
+// to the corresponding valid date.
+// 'month' is between 1 and 12 both inclusive. If month is out of range, a
+// 'panic' is raised.
+func NewTime(day, month, year, hour, min, sec int) T {
+	lc, err := time.LoadLocation("Local")
+	if err != nil {
+		panic(err)
+	}
+	return T(time.Date(year, toMonth(month), day, hour, min, sec, 0, lc))
+}
+
+// 's' is in format "yyyymmdd"
+// If there are some errors, raise 'panic'
+func FromString(s string) T {
+	d, err := time.Parse("20060102", s)
+	if err != nil {
+		panic(err)
+	}
+	return T(d)
+}
+
+// For 'sep' = "/", 's' is in format "dd/mm/yyyy"
+// If there are some errors, raise 'panic'
+func FromIso(s, sep string) T {
+	d, err := time.Parse("02"+sep+"01"+sep+"2006", s)
+	if err != nil {
+		panic(err)
+	}
+	return T(d)
+}
+
+// For 'sep' = "/", 's' is in format "mm/dd/yyyy"
+// If there are some errors, raise 'panic'
+func FromEn(s, sep string) T {
+	d, err := time.Parse("01"+sep+"02"+sep+"2006", s)
+	if err != nil {
+		panic(err)
+	}
+	return T(d)
+}
+
+func (d T) Day() int {
+	return time.Time(d).Day()
+}
+
+// Returns the week day. 0 -> Sunday ... 6 -> Saturday
+func (d T) Weekday() int {
+	switch time.Time(d).Weekday() {
+	case time.Sunday:
+		return 0
+	case time.Monday:
+		return 1
+	case time.Tuesday:
+		return 2
+	case time.Wednesday:
+		return 3
+	case time.Thursday:
+		return 4
+	case time.Friday:
+		return 5
+	default:
+		return 6
+	}
+}
+
+// In the range [1-12]
+func (d T) Month() int {
+	return fromMonth(time.Time(d).Month())
+}
+
+func (d T) Year() int {
+	return time.Time(d).Year()
+}
+
+func (d T) Hour() int {
+	return time.Time(d).Hour()
+}
+
+func (d T) Minute() int {
+	return time.Time(d).Minute()
+}
+
+func (d T) Second() int {
+	return time.Time(d).Second()
+}
+
+func (d T) Millisecond() int {
+	return time.Time(d).Nanosecond() / 1000000
+}
+
+func (d T) Add(days int) T {
+	return T(time.Time(d).Add(time.Hour * 24 * time.Duration(days)))
+}
+
+func (d T) AddSeconds(days int) T {
+	return T(time.Time(d).Add(time.Second * time.Duration(days)))
+}
+
+func (d T) AddMilliseconds(days int) T {
+	return T(time.Time(d).Add(time.Millisecond * time.Duration(days)))
+}
+
+// Milliseconds difference. d - other.
+func (d T) DfTime(other T) int {
+	return int(time.Time(d).Sub(time.Time(other)).Milliseconds())
+}
+
+// Days difference. d - other.
+func (d T) Df(other T) int {
+	df := d.DfTime(other)
+	dv := df / 86400000
+	if df >= 0 {
+		if df%86400000 >= 43200000 {
+			dv++
+		}
+	} else {
+		if df%86400000 <= -43200000 {
+			dv--
+		}
+	}
+
+	return dv
+}
+
+// Compare up to day.
+func (d T) Eq(other T) bool {
+	return d.Day() == other.Day() &&
+		d.Month() == other.Month() &&
+		d.Year() == other.Year()
+}
+
+// Compare up to milliseconds.
+func (d T) EqTime(other T) bool {
+	return d.Day() == other.Day() &&
+		d.Month() == other.Month() &&
+		d.Year() == other.Year() &&
+		d.Hour() == other.Hour() &&
+		d.Minute() == other.Minute() &&
+		d.Second() == other.Second() &&
+		d.Millisecond() == other.Millisecond()
+}
+
+// Compare up to day
+func (d T) Compare(other T) int {
+	if d.Year() > other.Year() {
+		return 1
+	}
+	if d.Year() < other.Year() {
+		return -1
+	}
+	if d.Month() > other.Month() {
+		return 1
+	}
+	if d.Month() < other.Month() {
+		return -1
+	}
+	if d.Day() > other.Day() {
+		return 1
+	}
+	if d.Day() < other.Day() {
+		return -1
+	}
+	return 0
+}
+
+// Compare up to milliseconds.
+func (d T) CompareTime(other T) int {
+	cmp := d.Compare(other)
+	if cmp == 0 {
+		if d.Hour() > other.Hour() {
+			return 1
+		}
+		if d.Hour() < other.Hour() {
+			return -1
+		}
+		if d.Minute() > other.Minute() {
+			return 1
+		}
+		if d.Minute() < other.Minute() {
+			return -1
+		}
+		if d.Second() > other.Second() {
+			return 1
+		}
+		if d.Second() < other.Second() {
+			return -1
+		}
+		if d.Millisecond() < other.Millisecond() {
+			return -1
+		}
+		return 0
+	}
+	return cmp
+}
+
+// Interface to sort dates (Compare up to day). Example:
+//    sort.Sort(date.Sorter(sliceOfDates))
+type Sorter []T
+
+func (a Sorter) Len() int {
+	return len(a)
+}
+func (a Sorter) Less(i, j int) bool {
+	return a[i].Compare(a[j]) < 0
+}
+func (a Sorter) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+// Interface to sort dates (Compare up to milliseconds). Example:
+//    sort.Sort(date.Sorter(sliceOfDates))
+type SorterTime []T
+
+func (a SorterTime) Len() int {
+	return len(a)
+}
+func (a SorterTime) Less(i, j int) bool {
+	return a[i].CompareTime(a[j]) < 0
+}
+func (a SorterTime) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+// Returns a string that represents the date.
+// 'template' is a kind 'printf' with next sustitution variables:
+//    %d  Day in number 06 -> 6.
+//    %D  Day with tow digits 06 -> 06.
+//    %m  Month in number 03 -> 3.
+//    %M  Month with two digits 03 -> 03.
+//    %y  Year with two digits 2010 -> 10.
+//    %Y  Year with four digits 2010 -> 2010.
+//    %t  Time without milliseconds -> 15:03:55
+//    %T  Time with milliseconds -> 15:03:55.345
+//    %%  The sign '%'.
+func (d T) Format(tmp string) string {
+	tmp = strings.ReplaceAll(tmp, "%d", time.Time(d).Format("2"))
+	tmp = strings.ReplaceAll(tmp, "%D", time.Time(d).Format("02"))
+	tmp = strings.ReplaceAll(tmp, "%m", time.Time(d).Format("1"))
+	tmp = strings.ReplaceAll(tmp, "%M", time.Time(d).Format("01"))
+	tmp = strings.ReplaceAll(tmp, "%y", time.Time(d).Format("06"))
+	tmp = strings.ReplaceAll(tmp, "%Y", time.Time(d).Format("2006"))
+	tmp = strings.ReplaceAll(tmp, "%t", time.Time(d).Format("15:04:05"))
+	tmp = strings.ReplaceAll(tmp, "%T", time.Time(d).Format("15:04:05.000"))
+	tmp = strings.ReplaceAll(tmp, "%%", "%")
+	return tmp
+}
+
+// Returns a string in format 'yyyymmdd'.
+func (d T) String() string {
+	return d.Format("%Y%M%D")
+}
+
+// Returns a string in format 'dd/mm/yyyy'.
+func (d T) ToIso() string {
+	return d.Format("%D/%M/%Y")
+}
+
+// Returns a string in format 'mm/dd/yyyy'.
+func (d T) ToEn() string {
+	return d.Format("%M/%D/%Y")
+}
